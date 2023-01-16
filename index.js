5cb5cb51718b2ebeb1fdebb2b49b87e8307d794a
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-01-16 15:36:12 +0300

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+const max = 30;
+const min = 10;
+for (let i = max; (i) => min; i -= 1) {
+  if (i % 2 === 0) {
+  }
+  console.log(i);
+}
+
+// let counter = 0;
+// while (counter < 10) {
+//   console.log("counter:", counter);
+//   counter += 1;
+// }
