a4238b34740fa562c5886f06279e879302a198c0
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-01-21 09:38:28 +0300

    add

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const max = 30;
-const min = 10;
-for (let i = max; (i) => min; i -= 1) {
-  if (i % 2 === 0) {
-  }
-  console.log(i);
-}
+// const max = 30;
+// const min = 10;
+// for (let i = max; i >= min; i -= 1) {
+//   if (i % 2 === 0) {
+//   }
+//   console.log(i);
+// }
 
 // let counter = 0;
 // while (counter < 10) {
 //   console.log("counter:", counter);
 //   counter += 1;
 // }
+// const cart = [54, 28, 105, 70, 92, 17, 120];
+// let total = 0;
+// for (let i = 0; i < cart.length; i += 1) {
+//   // console.log(cart[i]);
+//   total += cart[i];
+// }
+// console.log(total);
+// const numbers = [5, 99, 309, 7, 754];
+// let biggestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number > biggestNumber) {
+//   }
+//   biggestNumber = number;
+// }
+
+// console.log(biggestNumber);
+
+// const numbers = [2, 17, 94, 1, 23, 37, -1, 100, -2];
+// let min = numbers[0];
+
+// for (const number of numbers) {
+//   if (min > number) {
+//     min = number;
+//   }
+// }
+
+const greet = function () {
+  console.log("Welcome");
+};
+greet();
